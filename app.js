41d67f0f6e36e92b31f0180ee059bb6bfaf77bd8
Dmitry <dmitnb@ya.ru>
2022-04-24 13:16:24 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const myName = 'Dima'
+
+myName = "Goga"
+
+console.log(myName)
+function
---
+++ script.js
@@ -0,0 +1,12 @@
+function humanSayHello(user) {
+    console.log('Hello ' + user + '!');
+}
+
+humanSayHello()
+
+function calc(a, b) {
+    console.log(a + b);
+}
+calc(4, 5);
+calc(14, 6);
+calc(8, 5);
